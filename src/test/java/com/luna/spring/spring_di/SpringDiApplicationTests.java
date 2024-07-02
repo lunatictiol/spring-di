c3c1d285480236f583f3a660e0d6e22f86e955b0
“sabiq” <sabiq.a@myclnq.co.in>
2024-07-02 16:07:12 +0530

    first commit

---
+++ src/test/java/com/luna/spring/spring_di/SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luna.spring.spring_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
